a6201201d078e6d8f1c71d93d38dbdb56ac2df52
ExgleGaming <167655468+ExgleGaming@users.noreply.github.com>
2025-07-13 14:07:14 -0400

    assignment complete

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -1,1 +1,82 @@
 import React from "react";
+import './EmployeeForm';
+
+class EmployeeForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state  = {name: '', email: '', title: '', department: ''};
+    }
+
+    handleChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Submitted Employee:', this.state);
+
+        this.setState({
+            name: '',
+            email: '',
+            title: '',
+            department: ''
+        });
+    };
+
+    render() {
+        const { name, email, title, department } = this.state;
+        return (
+            <form className="employee-form" onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                <label htmlFor="name">Name:</label>
+                <input
+                    id="name"
+                    name="name"
+                    type="text"
+                    value={name}
+                    onChange={this.handleChange}
+                />
+                </div>
+
+                <div className="form-group">
+                <label htmlFor="email">Email:</label>
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    value={email}
+                    onChange={this.handleChange}
+                />
+                </div>
+
+                <div className="form-group">
+                <label htmlFor="title">Job Title:</label>
+                <input
+                    id="title"
+                    name="title"
+                    type="text"
+                    value={title}
+                    onChange={this.handleChange}
+                />
+                </div>
+
+                <div className="form-group">
+                <label htmlFor="department">Department:</label>
+                <input
+                    id="department"
+                    name="department"
+                    type="text"
+                    value={department}
+                    onChange={this.handleChange}
+                />
+                </div>
+
+                <button type="submit">Submit</button>
+            </form>
+        );
+    
+    }
+}
+
+export default EmployeeForm;
